f3de3d5aea607c344a7af0f6a6192bd77dfecd0c
Aline <alinebora@gmail.com>
2022-04-13 17:34:55 +0200

    Adding time clock

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
 let apiKey = "384781b633046620eaed677419a0ac6e";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=${apiKey}&units=metric`;
 
+function formatDate(timeStamp) {
+    let date = new Date(timeStamp);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = weekdays[date.getDay()];
+    return `${day} ${hours} ${minutes}`;
+}
+
 function displayTemperature(response) {
     document.querySelector("#temperature-degree").innerHTML = Math.round(response.data.main.temp);
     document.querySelector("#city").innerHTML = response.data.name;
@@ -8,6 +20,7 @@ function displayTemperature(response) {
     document.querySelector("#feels-like").innerHTML = Math.round(response.data.main.feels_like);
     document.querySelector("#humidity").innerHTML = response.data.main.humidity;
     document.querySelector("#wind-speed").innerHTML = Math.round(response.data.wind.speed);
+    document.querySelector("#date").innerHTML = formatDate(response.data.dt * 1000);
 }
 
 axios.get(apiUrl).then(displayTemperature);
