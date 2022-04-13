e3364f84f2d209110faaacec7b65b39b4c554f81
Aline <alinebora@gmail.com>
2022-04-13 17:12:42 +0200

    Addin api integration

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
-let apiKey: "384781b633046620eaed677419a0ac6e";
-let apiUrl: `https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
+let apiKey = "384781b633046620eaed677419a0ac6e";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=${apiKey}&units=metric`;
+
+function displayTemperature(response) {
+    document.querySelector("#temperature-degree").innerHTML = Math.round(response.data.main.temp);
+    document.querySelector("#city").innerHTML = response.data.name;
+    document.querySelector("#current-condition").innerHTML = response.data.weather[0].description;
+    document.querySelector("#feels-like").innerHTML = Math.round(response.data.main.feels_like);
+    document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+    document.querySelector("#wind-speed").innerHTML = Math.round(response.data.wind.speed);
+}
+
+axios.get(apiUrl).then(displayTemperature);
