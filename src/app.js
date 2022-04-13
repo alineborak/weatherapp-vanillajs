9c824964aa2ab9d566ba833fb9c295cd3068a043
Aline <alinebora@gmail.com>
2022-04-13 20:06:17 +0200

    Added weather with api

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 let apiKey = "384781b633046620eaed677419a0ac6e";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=${apiKey}&units=metric`;
+let city = "Amsterdam";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 function formatDate(timeStamp) {
     let date = new Date(timeStamp);
     let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
     let minutes = date.getMinutes();
     if (minutes < 10) {
         minutes = `0${minutes}`;
@@ -20,6 +24,7 @@ function displayTemperature(response) {
     document.querySelector("#feels-like").innerHTML = Math.round(response.data.main.feels_like);
     document.querySelector("#humidity").innerHTML = response.data.main.humidity;
     document.querySelector("#wind-speed").innerHTML = Math.round(response.data.wind.speed);
+    document.querySelector("#icon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     document.querySelector("#date").innerHTML = formatDate(response.data.dt * 1000);
 }
 
